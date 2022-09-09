9d83c17583245766f76ae4b6e0da58bc212ecf2c
João Bernardo Del Rio <54190405+JheyBi@users.noreply.github.com>
2022-09-09 15:03:57 -0300

    Criado No ex2

---
+++ Prova 2/Ex2/No.java
@@ -0,0 +1,29 @@
+public class ListaDinamica{
+    No inicio;
+    No fim;
+
+    
+    public void insere(No n){
+        if (inicio == null){
+            inicio = n;
+            fim = n;
+        }
+        else{
+            fim.setProximo(n);
+            n.setAnterior(fim);
+            fim = n;
+        }
+    }
+
+ 
+    public void imprime(){
+        No aux = inicio;
+        while (aux!=null){
+            System.out.println(aux.toString());
+            aux = aux.getProximo();
+        }
+
+    }
+
+
+}
